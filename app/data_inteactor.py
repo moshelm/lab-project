893dbb243730abc7c9145546bbc72f8161906427
Moshe <moshe@example.com>
2025-12-23 15:49:30 +0200

    finish db mysql

--- app/data_inteactor.py
+++ app/data_inteactor.py
@@ -12,15 +12,51 @@ class Database:
     def create_database(self):
         sql = f"CREATE DATABASE IF NOT EXISTS `{self.name}`;"
         self.cursor.execute(sql)
+        self.conn.database = self.name
         return self.name
 
     def create_table(self, table_name:str, fields:tuple[str]):
         fields = ',\n'.join(fields)
         if not fields:
             raise ValueError
-        sql = f"""USE {self.name};
+        sql = f"""
         CREATE TABLE IF NOT EXISTS `{table_name}` (
 id INT AUTO_INCREMENT PRIMARY KEY,
 {fields});"""
 
         self.cursor.execute(sql)
+
+    def create_contact(self,table_name:str, info:dict):
+        fields = ','.join(list(info.keys()))
+        values = tuple(info.values())
+        flags = ','.join(['%s' for _ in range(len(list(info.keys())))])
+        sql = (f"""
+               INSERT INTO `{table_name}` ({fields})
+               VALUES ({flags});""")
+        self.cursor.execute(sql,values)
+        self.conn.commit()
+
+    def get_all(self,table_name:str):
+        sql = f"USE `{self.name}`;SELECT * FROM `{table_name}`;"
+        self.cursor.execute(sql)
+        result = [row for row in self.cursor]
+        print(result)
+        return result
+
+    def update_record(self,table_name:str,record_id:int, update_info:dict):
+        updates = []
+        for field in list(update_info.keys()):
+            updates.append(f"{field} = %s")
+
+        sql = f"""UPDATE  `{table_name}` SET {','.join(updates)} WHERE id = %s;"""
+        values = tuple(list(update_info.values()).append(record_id))
+
+        self.cursor.execute(sql,values)
+        self.conn.commit()
+
+    def delete_record(self,table_name:str, record_id:int):
+        sql = f"DELETE FROM {table_name} WHERE id = %s"
+        value = record_id
+        self.cursor.execute(sql,value)
+        self.conn.commit()
+
