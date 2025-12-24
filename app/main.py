69237053d8c95c0be8ab94cfbfe6673679ffb101
Moshe <moshe@example.com>
2025-12-24 17:57:22 +0200

    finish project

--- app/main.py
+++ app/main.py
@@ -2,57 +2,57 @@ from fastapi import FastAPI, HTTPException, status
 from schemas_api import Contact
 import uvicorn
 from db_manager import DBContactsManager
-from pathlib import Path
+import os
 
 
-current_file = Path(__file__).resolve()
-sql_path = current_file.parent.parent / "sql" / "init.sql"
+# name of database. from env.
+DATABASE = os.getenv('DB_NAME','contacts_db')
 
-DATABASE = 'contacts_db'
 TABLE_NAME = 'contacts'
-
-INITIALIZE_SQL = sql_path
-
 
 
 app = FastAPI()
 
-db = DBContactsManager(file_name=INITIALIZE_SQL)
+db = DBContactsManager(database_name=DATABASE)
 
 @app.get('/contacts')
 def get_all_contacts():
-    
-    contacts =  db.database.get_all(TABLE_NAME)
-    
-    
-    if not contacts:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"There is no contacts"
-        )
-    else:
+    try:
+        contacts =  db.database.get_all(TABLE_NAME)
+        if not contacts:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"There is no contacts")
         return contacts
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Database connection error: {str(e)}")
+
 
 @app.post('/contacts')
 def create_contact(contact:Contact):
-    d_contact = contact.model_dump()
-    new_id = db.database.create_contact(TABLE_NAME, d_contact)
-    return {'message':"Contact created successfully",
-            'id':new_id}
-
+    try:
+        contact_py = contact.model_dump()
+        new_id = db.database.create_contact(TABLE_NAME, contact_py)
+        return {'message':"Contact created successfully",
+                'id':new_id}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Could not create contact: {str(e)}")
 
 @app.put('/contacts/{id_contact}')
 def update_contact(id_contact:int, new_info:Contact):
     contact = new_info.model_dump()
-    db.database.update_record(TABLE_NAME, id_contact, contact)
-    return {"status": "update"}
+    if db.database.update_record(TABLE_NAME, id_contact, contact):
+        return {"status": "update"}
+    else:
+        return {"status":"error"}    
+
 
 
 @app.delete('/contacts/{id_contact}')
 def delete_contact(id_contact:int):
-    db.database.delete_record(TABLE_NAME, id_contact)
-    return {"status": "deleted"}
-
-
-if __name__ == '__main__':
-    uvicorn.run(app,host='127.0.0.1',port=8000)
+    
+    if db.database.delete_record(TABLE_NAME, id_contact):
+        return {"status": "deleted"}
+    else:
+        return {"status":"error"}    
+    
---
+++ sql/README.md
@@ -0,0 +1,7 @@
+project  contacts manager 
+
+in main.py we are runing the manger for this project in db_manager.py 
+the manager create database using data_inteactor.py
+data_inteactor.py:  in thise file you have the connector to database mysql and CURD funcations 
+
+impoertent :you need to run service-api after using in compose to run it (mining runing tiwes in desktop)
